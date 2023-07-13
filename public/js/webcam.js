dfc513c63e610a4be68d4d3640afb7ca1bba46f5
Mekafuyu <mayconbrunob@hotmail.com>
2023-07-13 12:50:25 -0300

    Resizing and add func to handle print

--- public/js/webcam.js
+++ public/js/webcam.js
@@ -1,13 +1,16 @@
 const webcamElement = document.getElementById("webcam");
 const canvasElement = document.getElementById("canvas");
 const snapSoundElement = document.getElementById("snapSound");
+
 const webcam = new Webcam(
   webcamElement,
   "user",
   canvasElement,
   snapSoundElement
 );
+
 webcam.flip();
+
 webcam
   .start()
   .then((result) => {
@@ -17,25 +20,73 @@ webcam
     console.log(err);
   });
 
-document.querySelector("#download-photo").href = picture;
 
-function saveasimg() {
+
+
+
+// function saveasimg() {
+//   canvasElement.toBlob((blob) => {
+//     let file = new File([blob], "fileName.jpg", { type: "image/jpeg" });
+    
+//     var reader = new FileReader();
+    
+//     reader.onload = function (e) {
+//       var image = document.createElement("img");
+      
+//       var divimg = document.getElementById("cam");
+      
+//       image.src = e.target.result;
+//       divimg.appendChild(image);
+//     };
+//     // you have to declare the file loading
+//     reader.readAsDataURL(file);
+    
+//     console.log(file);
+//   }, "image/jpeg");
+// }
+// function saveasimg() {
+
+//   canvasElement.toBlob((blob) => {
+//     let file = new File([blob], "fileName.jpg", { type: "image/jpeg" });
+//     var reader = new FileReader();
+    
+//     reader.onload = function (e) {
+//       var image = document.createElement("img");
+      
+//       var divimg = document.getElementById("cam");
+      
+//       image.src = e.target.result;
+//       divimg.appendChild(image);
+//     };
+//     // you have to declare the file loading
+//     reader.readAsDataURL(file);
+    
+//     console.log(file);
+//   }, "image/jpeg");
+// }
+
+
+$(' .print ').on( 'click', (event) => {
+  // console.log($(e.target).parent().parent().append())
+  webcam.snap();
   canvasElement.toBlob((blob) => {
     let file = new File([blob], "fileName.jpg", { type: "image/jpeg" });
-    // document.getElementById("foto").setAttribute(file)
-
     var reader = new FileReader();
-    // it's onload event and you forgot (parameters)
+    
     reader.onload = function (e) {
       var image = document.createElement("img");
-      var divimg = document.getElementById("cam");
-      // the result image data
+      
+      // var divimg = document.getElementById("cam");
+      
       image.src = e.target.result;
-      divimg.appendChild(image);
+
+      console.log(event.target);
+      $(event.target).parent().parent().append(image);
     };
     // you have to declare the file loading
     reader.readAsDataURL(file);
-
+    
     console.log(file);
   }, "image/jpeg");
-}
+
+})
